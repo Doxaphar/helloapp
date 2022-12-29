2bcf47596c6cb9f01234728e67dbd0e0af0b4c8a
black <black@example.com>
2022-12-29 16:50:10 +0500

    hello everyone

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("hello everyone");
         }
 
         private static void GreetWhite()
