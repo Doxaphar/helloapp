c4110a268c7b2ee2fbb23f23009286e4533f0a6c
black <black@example.com>
2022-12-29 17:04:40 +0500

    Some fixes

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -7,7 +7,10 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("hello everyone");
+			GreetWhite();
+			GreetBlack();
         }
+
         private static void GreetWhite()
         {
             Console.WriteLine("hello white");
